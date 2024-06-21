e3f9fa31b324148ba3a3da868c6d3275fb56ea9d
Will Hack <18036720+willhack@users.noreply.github.com>
2024-06-20 21:58:05 -0400

    remove buggy lint plugin

--- init.lua
+++ init.lua
@@ -768,7 +768,6 @@ require('lazy').setup({
   --
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.gitsigns',
-  require 'kickstart.plugins.lint',
   require 'kickstart.plugins.neo-tree',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
